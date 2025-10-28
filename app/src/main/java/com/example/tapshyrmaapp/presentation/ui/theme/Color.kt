df700715a38aab1fa162c378b236d2a1565eea5a
btoktogaziev <btoktogaziev@nurtelecom.kg>
2025-10-28 15:16:04 +0600

    Работа с цветами

---
+++ app/src/main/java/com/example/tapshyrmaapp/presentation/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.tapshyrmaapp.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundMagenta = Color(0xFFF0047F)
+val BackgroundBurgundy = Color(0xFFC20344)
+val BackgroundRed = Color(0xFFF0044F)
+
+val BackgroundPink = Color(0xFFFF74AB)
